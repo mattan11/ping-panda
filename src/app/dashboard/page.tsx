0bed0b5f7ebd181f6ad771ad304d79f8b97fa632
Marek Witkowski <marek.witkowski@intersynergy.pl>
2024-11-25 15:02:15 +0100

    add dashboard page and list of categories

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,60 @@
+// import { DashboardPage } from "@/components/dashboard-page"
+import { db } from "@/db"
+import { currentUser } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation"
+// import { DashboardPageContent } from "./dashboard-page-content"
+// import { CreateEventCategoryModal } from "@/components/create-event-category-modal"
+import { Button } from "@/components/ui/button"
+import { PlusIcon } from "lucide-react"
+import { DashboardPage } from "@/components/dashboard-page"
+import { DashboardPageContent } from "@/app/dashboard/dashboard-page-content"
+
+// import { createCheckoutSession } from "@/lib/stripe"
+// import { PaymentSuccessModal } from "@/components/payment-success-modal"
+
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined
+  }
+}
+
+const Page = async ({ searchParams }: PageProps) => {
+  const auth = await currentUser()
+
+  if (!auth) {
+    redirect("/sign-in")
+  }
+
+  const user = await db.user.findUnique({
+    where: { externalId: auth.id },
+  })
+
+  if (!user) {
+    return redirect("/welcome")
+  }
+
+  const intent = searchParams.intent
+
+  // if (intent === "upgrade") {
+  //   const session = await createCheckoutSession({
+  //     userEmail: user.email,
+  //     userId: user.id,
+  //   })
+  //
+  //   if (session.url) redirect(session.url)
+  // }
+
+  const success = searchParams.success
+
+  return (
+    <>
+      {/*{success ? <PaymentSuccessModal /> : null}*/}
+
+      <DashboardPage title="Dashboard" hideBackButton={true}>
+        <DashboardPageContent />
+      </DashboardPage>
+    </>
+  )
+}
+
+export default Page
